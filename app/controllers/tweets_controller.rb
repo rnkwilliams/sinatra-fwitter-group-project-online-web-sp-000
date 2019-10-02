576321b17258e7036a9b1c70230f783e353047c2
Ranika Williams <rnkprice07@gmail.com>
2019-10-02 15:19:39 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -43,7 +43,7 @@ class TweetsController < ApplicationController
    end
    
    get 'tweets/:id/edit' do
-      if Helpers.is_logged_in?(session)
+      if !Helpers.is_logged_in?(session)
         redirect to '/login'
       end
       @tweet = Tweet.find(params[:id])
